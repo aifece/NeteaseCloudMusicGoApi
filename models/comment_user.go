1744e65502ab2cfb612d819c604942e4dbe656c5
wusongjian <wusongjian@sanjieke.com>
2020-11-30 15:34:34 +0800

    use sync map

--- models/comment_user.go
+++ models/comment_user.go
@@ -1,15 +1,25 @@
 package models
 
-import (
-	"fmt"
-	"sync"
-	"time"
-	"strconv"
-	"github.com/aifece/NeteaseCloudMusicGoApi/pkg/request"
-)
 
+import (
+	_ "sync"
+)
 func (m *MusicObain) CommentUser(query map[string]interface{}) map[string]interface{} {
-	result := getUserCommentList(query)
+	uid := ""
+	if val, ok := query["uid"].(string); ok {
+		uid = val
+	}
+	id := ""
+	if val, ok := query["id"].(string); ok {
+		id = val
+	}
+	key := uid + "_" + id
+	result := []interface{}{}
+	if userCommentList, ok := userComment.Load(key); ok {
+		for _, v := range userCommentList.(map[int]interface{}) {
+			result = append(result, v)
+		}
+	}
 
 	answer := map[string]interface{}{
 		"status": 200,
@@ -23,226 +33,4 @@ func (m *MusicObain) CommentUser(query map[string]interface{}) map[string]interf
 	}
 
 	return answer
-}
-
-var userComment map[string][]map[string]interface{}
-var userCookie map[string]interface{}
-func getUserCommentList(query map[string]interface{}) []map[string]interface{} {
-	uid := ""
-	if val, ok := query["uid"].(string); !ok {
-		return []map[string]interface{}{}
-	} else {
-		uid = val
-	}
-	if userComment == nil {
-		userComment = make(map[string][]map[string]interface{})
-	}
-	if queryData == nil {
-		queryData = make(map[string]map[string]int)
-	}
-	if userCookie == nil {
-		userCookie = make(map[string]interface{})
-	}
-	if val, ok := query["cookie"]; ok {
-		userCookie = val.(map[string]interface{})
-	}
-
-	userCommentList, ok := userComment[uid]
-	if ok {
-		return userCommentList
-	} else {
-		if queryData == nil {
-			queryData = make(map[string]map[string]int)
-		}
-		queryData[uid] = map[string]int{ "page": 1, "beforeTime": 0 }
-		go timeoutComment(query)
-		var wg = sync.WaitGroup{}
-		stopChan := make(chan bool, 1)
-		wg.Add(1)
-		go runGetCommentList(stopChan, &wg, query)
-
-		return []map[string]interface{}{}
-	}
-}
-
-func runGetCommentList(stopChan chan bool, wg *sync.WaitGroup, query map[string]interface{}) {
-	defer wg.Done()
-	uid, _ := query["uid"].(string)
-	if userComment[uid] == nil {
-		userComment[uid] = []map[string]interface{}{}
-	}
-	old_list := userComment[uid]
-	for {
-		more, list := getCommentList(query)
-		for _, v := range list {
-			old_list = append(old_list, v)
-		}
-		if !more {
-			break
-		}
-	}
-	userComment[uid] = old_list
-}
-
-var queryData map[string]map[string]int
-func getCommentList(query map[string]interface{}) (bool, []map[string]interface{}) {
-	defer func() {
-        if r := recover(); r != nil {
-            fmt.Printf("Play Recode Error：%s\n", r)
-        }
-    }()
-	uid, _ := query["uid"].(string)
-	userQueryData := queryData[uid]
-	data := map[string]interface{}{
-		"rid": query["id"],
-		"offset": (userQueryData["page"] - 1) * 50,
-		"limit": 50,
-		"beforeTime": userQueryData["beforeTime"],
-	}
-	fmt.Println("Run Commnet List", uid, data)
-
-	cookie := userCookie
-	cookie["od"] = "pc"
-
-	options := map[string]interface{}{
-		"crypto": "weapi",
-		"cookie": cookie,
-		"proxy":  query["proxy"],
-	}
-
-	resp := request.CreateRequest(
-		"POST", "https://music.163.com/api/v1/resource/comments/R_SO_4_"+fmt.Sprintf("%v", query["id"]),
-		data,
-		options)
-
-	list := []map[string]interface{}{}
-	body, body_ok := resp["body"].(map[string]interface{})
-	if !body_ok {
-		return false, list
-	}
-	cookie, cookie_ok := resp["cookie"].(map[string]interface{})
-	if cookie_ok {
-		userCookie = cookie
-	}
-	more, has_ok := body["more"].(bool)
-	if !has_ok {
-		return false, list
-	}
-	comments, data_ok := body["comments"].([]interface{})
-	if !data_ok {
-		return false, list
-	}
-	if more {
-		userQueryData["page"] = userQueryData["page"] + 1
-	}
-
-	for _, v := range comments {
-		commentItem := v.(map[string]interface{})
-		time, time_ok := commentItem["time"].(int)
-		if time_ok {
-			userQueryData["beforeTime"] = time
-		}
-		if user, ok := commentItem["user"].(map[string]interface{}); ok {
-			user_id, _ := user["userId"].(float64)
-			if uid == strconv.Itoa(int(user_id)) {
-				list = append(list, commentItem)
-			} else {
-				if commentId, ok := commentItem["commentId"].(float64); ok {
-					floorList := getFloorCommentList(query, int(commentId))
-					for _, v := range floorList {
-						list = append(list, v)
-					}
-				}
-			}
-		}
-	}
-	queryData[uid] = userQueryData
-
-	return more, list
-}
-
-func getFloorCommentList(query map[string]interface{}, commentId int) []map[string]interface{} {
-	defer func() {
-        if r := recover(); r != nil {
-            fmt.Printf("Play Recode Error：%s\n", r)
-        }
-    }()
-	options := map[string]interface{}{
-		"crypto": "weapi",
-		"cookie": userCookie,
-		"proxy":  query["proxy"],
-	}
-	data := map[string]interface{}{
-		"parentCommentId": commentId,
-		"threadId": "R_SO_4_" + query["id"].(string),
-		"limit": 20,
-	}
-
-	request_time := -1
-	list := []map[string]interface{}{}
-	uid, _ := query["uid"].(string)
-	for {
-		data["time"] = request_time
-		resp := request.CreateRequest(
-			"POST", "https://music.163.com/api/resource/comment/floor/get",
-			data,
-			options)
-
-		body, body_ok := resp["body"].(map[string]interface{})
-		if !body_ok {
-			break
-		}
-		cookie, cookie_ok := resp["cookie"].(map[string]interface{})
-		if cookie_ok {
-			userCookie = cookie
-		}
-		data, data_ok := body["data"].(map[string]interface{})
-		if !data_ok {
-			break
-		}
-		more, has_ok := data["hasMore"].(bool)
-		if !has_ok {
-			break
-		}
-		comments, comment_ok := data["comments"].([]interface{})
-		if !comment_ok {
-			break
-		}
-		for _, v := range comments {
-			commentItem := v.(map[string]interface{})
-			time, time_ok := commentItem["time"].(float64)
-			if time_ok {
-				request_time = int(time)
-			}
-			if user, ok := commentItem["user"].(map[string]interface{}); ok {
-				user_id, _ := user["userId"].(float64)
-				if uid == strconv.Itoa(int(user_id)) {
-					list = append(list, commentItem)
-				}
-			}
-		}
-		if !more {
-			break
-		}
-	}
-
-	return list
-}
-
-func timeoutComment(query map[string]interface{}) {
-	fmt.Println("Run Query Comment Job")
-	tick := time.NewTicker(60 * time.Second)
-	var wg = sync.WaitGroup{}
-	stopChan := make(chan bool, 1)
-	for {
-		wg.Add(1)
-		select {
-		case <-tick.C:
-			go runGetCommentList(stopChan, &wg, query)
-		case <-stopChan:
-			goto END
-		}
-	}
-	tick.Stop()
-END:
 }
--- models/user_playing.go
+++ models/user_playing.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"sync"
 	"time"
-
+	"strconv"
 	"github.com/aifece/NeteaseCloudMusicGoApi/pkg/request"
 )
 
@@ -24,22 +24,26 @@ func (m *MusicObain) UserPlaying(query map[string]interface{}) map[string]interf
 	return answer
 }
 
-var cacheMap map[string][]interface{}
-var cacheResultMap map[string]interface{}
-
+var cacheMap sync.Map
+var cacheResultMap sync.Map
+var userPlayingCookie sync.Map
+var userPlayingProxy sync.Map
+var userComment sync.Map
+var queryData sync.Map
 func setResultList(uid string) {
-	record_list, ok := cacheMap[uid]
-	if ok {
+	if val, ok := cacheMap.Load(uid); ok {
+		record_list := val.([]map[int]interface{})
 		i := len(record_list) - 1
-		new_list, ok := record_list[i].(map[int]interface{})
-		if !ok {
+		new_list := record_list[i]
+		if len(new_list) == 0 {
 			fmt.Println("Not found list", i)
 			return
 		}
 		result_list := make(map[int]interface{})
 		is_change := false
 		for ; i > 0; i-- {
-			prev_list, has_prev_list := record_list[i-1].(map[int]interface{})
+			prev_list := record_list[i-1]
+			has_prev_list := len(prev_list) > 0
 			for id, v := range new_list {
 				new_item := v.(map[string]interface{})
 				new_index := new_item["index"].(int)
@@ -81,11 +85,19 @@ func setResultList(uid string) {
 		}
 		now := time.Now()
 		check_time := fmt.Sprintf("%d-%d-%d %d:%d:%d", now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second())
-		user_result, has_record := cacheResultMap[uid].(map[string]interface{})
+
+		user_result := map[string]interface{}{}
+		has_record := false
+		if val, ok := cacheResultMap.Load(uid); ok {
+			user_result = val.(map[string]interface{})
+			has_record = true
+		}
+
 		var change_list []map[string]interface{}
+		sort_result_list := getSortResult(result_list)
 		change_item := map[string]interface{}{
 			"change_time": check_time,
-			"changes":     getSortResult(result_list),
+			"changes":     sort_result_list,
 		}
 		if has_record {
 			change_list = user_result["result"].([]map[string]interface{})
@@ -103,17 +115,23 @@ func setResultList(uid string) {
 				min = 1
 			}
 			change_list = append(change_list[min:max], change_item)
-			cacheResultMap[uid] = map[string]interface{}{
+			cacheResultMap.Store(uid, map[string]interface{}{
 				"is_change":  is_change,
 				"check_time": check_time,
 				"result":     change_list,
+			})
+			// 获取Ta的评论
+			for _, v := range sort_result_list {
+				if id, ok := v["id"].(int); ok {
+					go getUserCommentList(strconv.Itoa(id), uid)
+				}
 			}
 		} else {
-			cacheResultMap[uid] = map[string]interface{}{
+			cacheResultMap.Store(uid, map[string]interface{}{
 				"is_change":  is_change,
 				"check_time": check_time,
 				"result":     change_list,
-			}
+			})
 		}
 	}
 }
@@ -152,20 +170,19 @@ func getUserPlayList(query map[string]interface{}) (map[string]interface{}, erro
 	if val, ok := query["uid"]; ok {
 		query["uid"] = val
 	}
-	if cacheMap == nil {
-		cacheMap = make(map[string][]interface{})
-	}
-	if cacheResultMap == nil {
-		cacheResultMap = make(map[string]interface{})
-	}
-	if userPlayingCookie == nil {
-		userPlayingCookie = make(map[string]interface{})
-	}
 	if val, ok := query["cookie"]; ok {
-		userPlayingCookie = val.(map[string]interface{})
+		for k, v := range val.(map[string]interface{}) {
+			userPlayingCookie.Store(k, v)
+		}
+	}
+	userPlayingCookie.Store("od", "pc")
+	if val, ok := query["proxy"]; ok {
+		for k, v := range val.(map[string]interface{}) {
+			userPlayingProxy.Store(k, v)
+		}
 	}
 	uid := string(query["uid"].(string))
-	_, ok := cacheMap[uid]
+	_, ok := cacheMap.Load(uid)
 	if !ok {
 		go timeout(query)
 		var wg = sync.WaitGroup{}
@@ -174,13 +191,13 @@ func getUserPlayList(query map[string]interface{}) (map[string]interface{}, erro
 		go runGetList(stopChan, &wg, query)
 		wg.Wait()
 	}
-	_, second_ok := cacheMap[uid]
+	_, second_ok := cacheMap.Load(uid)
 	if second_ok {
-		result, ok := cacheResultMap[uid].(map[string]interface{})
-		if !ok {
+		if result, ok := cacheResultMap.Load(uid); ok {
+			return result.(map[string]interface{}), nil
+		} else {
 			return nil, errors.New("还需要一些时间...")
 		}
-		return result, nil
 	} else {
 		return nil, errors.New("正在启动...")
 	}
@@ -191,7 +208,8 @@ func runGetList(stopChan chan bool, wg *sync.WaitGroup, query map[string]interfa
 
 	uid := string(query["uid"].(string))
 	item := getRecordList(query)
-	old_list, ok := cacheMap[uid]
+	list, _ := cacheMap.Load(uid)
+	old_list, ok := list.([]map[int]interface{})
 	if len(item) == 0 && !ok {
 		stopChan <- true
 		fmt.Println("Run Playing Error:", uid)
@@ -201,34 +219,15 @@ func runGetList(stopChan chan bool, wg *sync.WaitGroup, query map[string]interfa
 	if ok {
 		old_list = old_list[1:]
 	} else {
-		old_list = make([]interface{}, 2)
+		old_list = make([]map[int]interface{}, 2)
 		old_list[1] = item
 	}
 	old_list = append(old_list, item)
 
-	cacheMap[uid] = old_list
+	cacheMap.Store(uid, old_list)
 	setResultList(uid)
 }
 
-func timeout(query map[string]interface{}) {
-	fmt.Println("Run Query Job")
-	tick := time.NewTicker(30 * time.Second)
-	var wg = sync.WaitGroup{}
-	stopChan := make(chan bool, 1)
-	for {
-		wg.Add(1)
-		select {
-		case <-tick.C:
-			go runGetList(stopChan, &wg, query)
-		case <-stopChan:
-			goto END
-		}
-	}
-	tick.Stop()
-END:
-}
-
-var userPlayingCookie map[string]interface{}
 func getRecordList(query map[string]interface{}) map[int]interface{} {
 	defer func() {
         if r := recover(); r != nil {
@@ -250,7 +249,7 @@ func getRecordList(query map[string]interface{}) map[int]interface{} {
 	options := map[string]interface{}{
 		"crypto": "weapi",
 		"cookie": userPlayingCookie,
-		"proxy":  query["proxy"],
+		"proxy":  userPlayingProxy,
 	}
 	resp := request.CreateRequest(
 		"POST", "https://music.163.com/weapi/v1/play/record",
@@ -262,7 +261,9 @@ func getRecordList(query map[string]interface{}) map[int]interface{} {
 	}
 	cookie, cookie_ok := resp["cookie"].(map[string]interface{})
 	if cookie_ok {
-		userPlayingCookie = cookie
+		for k, v := range cookie {
+			userPlayingCookie.Store(k, v)
+		}
 	}
 	weekData, data_ok := body["weekData"].([]interface{})
 	if !data_ok {
@@ -284,4 +285,235 @@ func getRecordList(query map[string]interface{}) map[int]interface{} {
 	}
 
 	return list
+}
+
+
+func getUserCommentList(id string, uid string) {
+	key := uid + "_" + id
+	_, ok := userComment.Load(key)
+	if !ok {
+		queryData.Store(key, map[string]int{ "page": 1, "beforeTime": 0, "only_one": 0 })
+		go timeoutComment(id, uid)
+		var wg = sync.WaitGroup{}
+		wg.Add(1)
+		go runGetCommentList(&wg, id, uid)
+	}
+}
+
+func runGetCommentList(wg *sync.WaitGroup, id string, uid string) {
+	defer wg.Done()
+	defer func() {
+        if r := recover(); r != nil {
+            fmt.Printf("Run Get Comment Go Error：%s\n", r)
+        }
+    }()
+
+	key := uid + "_" + id
+	old_list := map[int]interface{}{}
+	if val, ok := userComment.Load(key); ok {
+		old_list = val.(map[int]interface{})
+	}
+	for {
+		more, list := getCommentList(id, uid)
+		if len(list) > 0 {
+			fmt.Println("User Has Comment: ", key)
+		}
+		for _, v := range list {
+			if commentId, ok := v["commentId"].(float64); ok {
+				old_list[int(commentId)] = v
+				userComment.Store(key, old_list)
+			}
+		}
+		if !more {
+			break
+		}
+	}
+	userComment.Store(key, old_list)
+}
+
+func getCommentList(id string, uid string) (bool, []map[string]interface{}) {
+	defer func() {
+        if r := recover(); r != nil {
+            fmt.Printf("Comment Recode Error：%s\n", r)
+        }
+    }()
+    key := uid + "_" + id
+    userQueryData := map[string]int{}
+	if val, ok := queryData.Load(key); ok {
+		userQueryData = val.(map[string]int)
+	}
+	data := map[string]interface{}{
+		"rid": id,
+		"offset": (userQueryData["page"] - 1) * 50,
+		"limit": 50,
+		"beforeTime": userQueryData["beforeTime"],
+	}
+
+	list := []map[string]interface{}{}
+	if userQueryData["page"] > 20 {
+		userQueryData["page"] = 1
+		userQueryData["only_one"] = 1
+		userQueryData["beforeTime"] = 0
+		queryData.Store(key, userQueryData)
+		return false, list
+	}
+
+	options := map[string]interface{}{
+		"crypto": "weapi",
+		"cookie": userPlayingCookie,
+		"proxy":  userPlayingProxy,
+	}
+
+	resp := request.CreateRequest(
+		"POST", "https://music.163.com/api/v1/resource/comments/R_SO_4_" + id,
+		data,
+		options)
+
+	body, body_ok := resp["body"].(map[string]interface{})
+	if !body_ok {
+		return false, list
+	}
+	cookie, cookie_ok := resp["cookie"].(map[string]interface{})
+	if cookie_ok {
+		for k, v := range cookie {
+			userPlayingCookie.Store(k, v)
+		}
+	}
+	more, has_ok := body["more"].(bool)
+	if !has_ok {
+		return false, list
+	}
+	if userQueryData["only_one"] == 1 {
+		more = false
+	}
+	comments, data_ok := body["comments"].([]interface{})
+	if !data_ok {
+		return false, list
+	}
+	if more {
+		userQueryData["page"] = userQueryData["page"] + 1
+	}
+
+	for _, v := range comments {
+		commentItem := v.(map[string]interface{})
+		if time, ok := commentItem["time"].(float64); ok {
+			userQueryData["beforeTime"] = int(time)
+		}
+		if user, ok := commentItem["user"].(map[string]interface{}); ok {
+			user_id, _ := user["userId"].(float64)
+			if uid == strconv.Itoa(int(user_id)) {
+				list = append(list, commentItem)
+			} else {
+				if commentId, ok := commentItem["commentId"].(float64); ok {
+					floorList := getFloorCommentList(id, uid, int(commentId))
+					for _, v := range floorList {
+						list = append(list, v)
+					}
+				}
+			}
+		}
+	}
+	queryData.Store(key, userQueryData)
+
+	return more, list
+}
+
+func getFloorCommentList(id string, uid string, commentId int) []map[string]interface{} {
+	defer func() {
+        if r := recover(); r != nil {
+            fmt.Printf("Floor Comment Recode Error：%s\n", r)
+        }
+    }()
+	options := map[string]interface{}{
+		"crypto": "weapi",
+		"cookie": userPlayingCookie,
+		"proxy":  userPlayingProxy,
+	}
+	data := map[string]interface{}{
+		"parentCommentId": commentId,
+		"threadId": "R_SO_4_" + id,
+		"limit": 20,
+	}
+
+	request_time := -1
+	list := []map[string]interface{}{}
+	for {
+		data["time"] = request_time
+		resp := request.CreateRequest(
+			"POST", "https://music.163.com/api/resource/comment/floor/get",
+			data,
+			options)
+
+		body, body_ok := resp["body"].(map[string]interface{})
+		if !body_ok {
+			break
+		}
+		cookie, cookie_ok := resp["cookie"].(map[string]interface{})
+		if cookie_ok {
+			for k, v := range cookie {
+				userPlayingCookie.Store(k, v)
+			}
+		}
+		data, data_ok := body["data"].(map[string]interface{})
+		if !data_ok {
+			break
+		}
+		more, has_ok := data["hasMore"].(bool)
+		if !has_ok {
+			break
+		}
+		comments, comment_ok := data["comments"].([]interface{})
+		if !comment_ok {
+			break
+		}
+		for _, v := range comments {
+			commentItem := v.(map[string]interface{})
+			time, time_ok := commentItem["time"].(float64)
+			if time_ok {
+				request_time = int(time)
+			}
+			if user, ok := commentItem["user"].(map[string]interface{}); ok {
+				user_id, _ := user["userId"].(float64)
+				if uid == strconv.Itoa(int(user_id)) {
+					list = append(list, commentItem)
+				}
+			}
+		}
+		if !more {
+			break
+		}
+	}
+
+	return list
+}
+
+func timeout(query map[string]interface{}) {
+	fmt.Println("Run Query Job")
+	tick := time.NewTicker(30 * time.Second)
+	var wg = sync.WaitGroup{}
+	stopChan := make(chan bool, 1)
+	for {
+		wg.Add(1)
+		select {
+		case <-tick.C:
+			go runGetList(stopChan, &wg, query)
+		case <-stopChan:
+			goto END
+		}
+	}
+	tick.Stop()
+END:
+}
+
+func timeoutComment(id string, uid string) {
+	fmt.Println("Run Query Comment Job")
+	tick := time.NewTicker(60 * time.Second)
+	var wg = sync.WaitGroup{}
+	for {
+		wg.Add(1)
+		select {
+		case <-tick.C:
+			go runGetCommentList(&wg, id, uid)
+		}
+	}
 }
